afbe3bfedf6c4ef9b9a61f05055fa1b06a1c9535
NizarBourzaim <nizar.bourzaim2@gmail.com>
2026-04-19 17:47:54 +0100

    fix: restore missing imports in login page

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import API_URL from "../../utils/api";
 import Link from "next/link";
 
 export default function LoginPage() {
